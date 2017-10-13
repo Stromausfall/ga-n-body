38bac2e12a958aa57bca9aca6450a26a4f03e6f9
Stromausfall <mail@matthias-auer.net>
2017-10-13 22:57:39 +0200

    initial commit with starter templates for backend and frontend

---
+++ backend/src/main/java/net/matthiasauer/ganbody/GaNBodyApplication.java
@@ -0,0 +1,12 @@
+package net.matthiasauer.ganbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaNBodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaNBodyApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/net/matthiasauer/ganbody/GaNBodyApplicationTests.java
@@ -0,0 +1,16 @@
+package net.matthiasauer.ganbody;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GaNBodyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
